c49f246ef9393875c41a29c37ff67a837eac3927
wujinbo <tuyn53@163.com>
2022-07-12 21:26:03 +0800

    fix: 修改打包后的相对地址

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  baseUrl: process.env.NODE_ENV === 'production'
-    ? '/whateat/'
-    : '/',
+  publicPath: './',
+  publicPath: process.env.NODE_ENV === 'production' ? '' : '/whateat',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  // baseUrl: process.env.NODE_ENV === 'production' ? '/whateat/' : '/',
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
 })
