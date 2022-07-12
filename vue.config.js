e810a007258da204b19882ba181734c5c7b680a3
wujinbo <tuyn53@163.com>
2022-07-12 21:12:30 +0800

    fix: 修改域名前缀同仓库

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  baseUrl: process.env.NODE_ENV === 'production'
+    ? '/whateat/'
+    : '/',
   transpileDependencies: true,
   lintOnSave: false
 })
